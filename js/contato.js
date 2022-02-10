74ae473a583c358cba204c2ab535df5a2e59f959
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-02-10 17:33:31 -0300

    contato

--- js/contato.js
+++
@@ -1,9 +0,0 @@
-function btjuazeiro() {
-    document.getElementById('map').setAttribute('src', 'https://maps.google.com/maps?q=e2s%20corretora%20de%20seguros&t=&z=17&ie=UTF8&iwloc=&output=embed');
-}
-
-function btassare() {
-    document.getElementById('map').setAttribute('src', 'https://maps.google.com/maps?q=assare%20zenir&t=&z=17&ie=UTF8&iwloc=&output=embed');
-}
-
-
--- js/navbar.js
+++ js/navbar.js
@@ -42,3 +42,15 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+
+  /*funcoes pagina contato*/
+function btjuazeiro() {
+    document.getElementById('map').setAttribute('src', 'https://maps.google.com/maps?q=e2s%20corretora%20de%20seguros&t=&z=17&ie=UTF8&iwloc=&output=embed');
+}
+
+function btassare() {
+    document.getElementById('map').setAttribute('src', 'https://maps.google.com/maps?q=assare%20zenir&t=&z=17&ie=UTF8&iwloc=&output=embed');
+}
+
+
