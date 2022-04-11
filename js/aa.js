7d196c9a626ce0b4dffe0ac17d7e6e42e6af6178
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-04-11 17:14:13 -0300

    commit

---
+++ js/aa.js
@@ -0,0 +1,20 @@
+var img = document.querySelector('.d-block');
+
+if (img.getAttribute('src')=='img/imagem01.png') {
+
+
+    
+document.getElementById('id-carousel').style.backgroundColor = 'black';
+
+}
+if (img.getAttribute('src')=='img/imagem05.png') {
+
+
+    
+    document.getElementById('id-carousel').style.backgroundColor = 'red';
+    
+    }
+/*
+alert(img.getAttribute('src'));
+img.setAttribute('src', 'img/aliro.png');*/
+
